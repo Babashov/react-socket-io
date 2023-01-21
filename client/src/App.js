a57dbb4f1c9a8495b4db94062e637e9022245b4a
Babashov Mahammad <babashov.info@gmail.com>
2023-01-21 15:46:31 +0400

    init method initialized in useEffect

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
+import { useEffect } from 'react';
 import './App.css';
 import Palette from './components/Palette';
-
+import {init} from './components/socketProvider'
 function App() {
+
+  useEffect(()=>{
+    init()
+  },[])
+
   return (
     <div className="App">
       <Palette/>
