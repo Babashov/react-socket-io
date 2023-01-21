73a5b847037842c469faf0fa56595553f98c24cd
Babashov Mahammad <babashov.info@gmail.com>
2023-01-21 15:47:19 +0400

    created socket init & send param to socket server

---
+++ client/src/components/socketProvider.js
@@ -0,0 +1,18 @@
+import io from 'socket.io-client'
+
+let socket;
+export const init = ()=>{
+    console.log('Connecting to server...')
+    socket = io('http://localhost:3001',{
+        transports:['websocket']
+    })
+
+    socket.on('connect',()=>{
+        console.log('Connected to server')
+    })
+
+}
+
+export const send = (color)=>{
+    socket.emit('newColor',color)
+}
