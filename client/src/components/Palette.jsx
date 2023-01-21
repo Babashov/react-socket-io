41a0ab8813580e53dc1216b2cbd0e48c2aaf8c62
Babashov Mahammad <babashov.info@gmail.com>
2023-01-21 16:09:52 +0400

    removed {color} value

--- client/src/components/Palette.jsx
+++ client/src/components/Palette.jsx
@@ -1,14 +1,12 @@
 import {useState} from 'react'
 import {send} from './socketProvider'
 
-function Palette() {
+function Palette({activeColor}) {
     const [color,setColor] = useState('#fff')
   return (
     <div className='palette'>
-        <input value={color} onChange={(e)=>setColor(e.target.value)} type="color" />
+        <input value={activeColor} onChange={(e)=>setColor(e.target.value)} type="color" />
         <button onClick={()=>send(color)}>Click</button>
-        <br/><br/>
-        {color}
     </div>
   )
 }
