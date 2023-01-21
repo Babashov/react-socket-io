5d01dd404a1fa3cc9c5ae229ddf054e742891246
Babashov Mahammad <babashov.info@gmail.com>
2023-01-21 15:46:56 +0400

    sending color to backend server

--- client/src/components/Palette.jsx
+++ client/src/components/Palette.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import {useState} from 'react'
+import {send} from './socketProvider'
 
 function Palette() {
+    const [color,setColor] = useState('#fff')
   return (
     <div className='palette'>
-        <input type="color" />
-        <button>Click</button>
+        <input value={color} onChange={(e)=>setColor(e.target.value)} type="color" />
+        <button onClick={()=>send(color)}>Click</button>
+        <br/><br/>
+        {color}
     </div>
   )
 }
